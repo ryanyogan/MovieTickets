9a026e59782cb7477c4f991a3b4f8c308750a68e
Ryan Yogan <ryanyogan@gmail.com>
2017-06-11 19:36:10 -0700

    Added a movie poster, styled the grid better

---
+++ src/MoviePoster.js
@@ -0,0 +1,68 @@
+import React, { Component, PropTypes } from 'react';
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { defaultStyles } from './styles';
+
+// The dimensions of the device
+const { width, height } = Dimensions.get('window');
+
+// How many posters will be in each row / column
+const cols = 3, rows = 3;
+
+export default class MoviePoster extends Component {
+  static propTypes = {
+    movie: PropTypes.object.isRequired,
+    onOpen: PropTypes.func.isRequired,
+  }
+
+  render() {
+    const { movie, movie: { title, genre, poster }, onOpen } = this.props;
+
+    return (
+      <TouchableOpacity
+        style={styles.container}
+        onPress={() => onOpen(movie)}
+      >
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: poster }} style={styles.image} />
+        </View>
+        <Text style={styles.title} numberOfLines={1}>{title}</Text>
+        <Text style={styles.genre} numberOfLines={1}>{genre}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 10,
+    marginBottom: 10,
+    height: (height - 20 - 20) / rows - 10,
+    width: (width - 10) / cols - 10,
+  },
+  imageContainer: {
+    flex: 1, // Take up all the window
+  },
+  image: {
+    borderRadius: 10,
+    ...StyleSheet.absoluteFillObject, // Fill all space in container
+  },
+  title: {
+    ...defaultStyles.text,
+    fontSize: 14,
+    marginTop: 4,
+  },
+  genre: {
+    ...defaultStyles.text,
+    color: '#BBBBBB',
+    fontSize: 12,
+    lineHeight: 14,
+  },
+});
+
--- src/Movies.js
+++ src/Movies.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import {
   ScrollView,
-  Text,
+  StyleSheet,
   View,
 } from 'react-native';
 import { movies } from './data';
+import MoviePoster from './MoviePoster';
 
 export default class Movies extends Component {
   render() {
     return (
-      <View>
-        <ScrollView>
-          {movies.map((movie, index) => <Text>{movie.title}</Text>)}
+      <View style={styles.container}>
+        <ScrollView
+          contentContainerStyle={styles.scrollContent}
+          // Hide all scroll bars / indicators
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+        >
+          {movies.map((movie, index) => <MoviePoster
+            movie={movie}
+            onOpen={this.openMovie}
+            key={index}
+           />)}
         </ScrollView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+  },
+  scrollContent: {
+    flexDirection: 'row', // arrange posters in rows
+    flexWrap: 'wrap', // allow multiple rows
+  },
+});
